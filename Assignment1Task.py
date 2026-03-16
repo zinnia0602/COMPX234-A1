9ae19a99576b45dab5a8fd52c494c054c1863ee0
zinnia0602 <zinnia2112014475@gmail.com>
2026-03-16 20:39:05 +0800

    Update Assignment1Task.py

--- Assignment1Task.py
+++ Assignment1Task.py
@@ -72,6 +72,7 @@ class Assignment1:
             self.outer.binary.acquire()
             self.outer.print_list.queuePrint(printerID)
             self.outer.binary.release()
+            self.outer.semaphore.release()
             print(f"Printer ID: {printerID} : now available")
             # Print from the queue
             self.outer.print_list.queuePrint(printerID)
